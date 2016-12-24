3ebb614ec86b73fb6e0920f69d611cf3dc7c76eb
gioknx <giovanikx@gmail.comx>
2016-12-23 23:13:23 -0200

    Fix orthography mistake and added readme

--- main.rb
+++ main.rb
@@ -10,8 +10,8 @@ require 'byebug'
 url = "http://www.casamineira.com.br/imovel/aluguel/apartamento-para-alugar-no-buritis-100m2-RS1800/AP0"
 
 
-all_real_state_hashs = []
-all_real_state_arrays = []
+all_real_estate_hashs = []
+all_real_estate_arrays = []
 (0..100).each do |n|
   begin
 
@@ -29,24 +29,24 @@ all_real_state_arrays = []
     # Demais info
     info = doc.css(".pinfo li strong")
 
-    real_state_info = Hash.new
-    real_state_info["codigo"] = n
+    real_estate_info = Hash.new
+    real_estate_info["codigo"] = n
 
-    real_state_info["area"] = info[0].text.strip
-    real_state_info["quartos"] = info[1].text.strip
-    real_state_info["banheiros"] = info[2].text.strip
-    real_state_info["suites"] = info[3].text.strip
-    real_state_info["vagas"] = info[4].text.strip
-    real_state_info["condominio"] = info[5].text.strip
-    real_state_info["endereco"] = info[6].text.strip
-    real_state_info["IPTU"] = info[7].text.strip
+    real_estate_info["area"] = info[0].text.strip
+    real_estate_info["quartos"] = info[1].text.strip
+    real_estate_info["banheiros"] = info[2].text.strip
+    real_estate_info["suites"] = info[3].text.strip
+    real_estate_info["vagas"] = info[4].text.strip
+    real_estate_info["condominio"] = info[5].text.strip
+    real_estate_info["endereco"] = info[6].text.strip
+    real_estate_info["IPTU"] = info[7].text.strip
 
 
-    real_state_info["titulo"] = title.strip
-    real_state_info["preco"] = price.strip
+    real_estate_info["titulo"] = title.strip
+    real_estate_info["preco"] = price.strip
 
-    all_real_state_hashs << real_state_info
-    all_real_state_arrays << real_state_info.map do |k,v|
+    all_real_estate_hashs << real_estate_info
+    all_real_estate_arrays << real_estate_info.map do |k,v|
       v
     end
 
@@ -70,7 +70,7 @@ CSV.open("output.csv", "wb") do |csv|
           "IPTU",
           "Titulo",
           "Preco"]
-  all_real_state_arrays.each do |s|
+  all_real_estate_arrays.each do |s|
     csv << s
   end
 end
